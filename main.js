5b33ed66a65c5b469ca11786bd0882c5bd5a3d11
Ishaan-2009 <95971642+Ishaan-2009@users.noreply.github.com>
2023-07-16 22:23:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+
+}
+
+function draw(){
+    circle(110, 250, 20);
+    rect(150, 290, 75, 25);
+    triangle(300, 300, 328, 272, 356, 300);
+
+}
